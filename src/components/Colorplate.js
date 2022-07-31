be26e21d0a780173a51f08a6369f67a522f0eccb
Ashu-Barnwal <ashuu.barnwal@gmail.com>
2022-08-01 01:04:15 +0530

    cleaning code

--- src/components/Colorplate.js
+++ src/components/Colorplate.js
@@ -1,26 +1,6 @@
 const Colorplate = ({bg, hex, cName}) => {
-    // const dColor = [
-    //     'black', 
-    //     '#000', 
-    //     '#000000',
-    //     'grey',
-    //     'blue',
-    //     'darkblue'
-    // ];
-    // const invColor = dColor.includes(bg.toLowerCase()) ? 'white' : 'black';
     return (
-        <div className={`plate${cName}`} style={{
-            // backgroundColor: bg,
-            // color: invColor,
-            // border: '2px solid ' + invColor,
-            // borderRadius: '15px',
-            // width: '14rem',
-            // height: '12rem',
-            // padding: '1rem',
-            // fontSize: '2rem',
-            // boxShadow: '1px 5px 7px #161514',
-            // overflowWrap: 'break-word'
-        }}>
+        <div className={`plate${cName}`}>
             <div className={`colorPlate${cName}`} style={{
                 backgroundColor: bg
             }}>
